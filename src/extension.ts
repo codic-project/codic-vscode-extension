139b91db927bd87995d892a2e374f08cdde3464d
pizzacat83 <pizzacat83.cpp@gmail.com>
2016-12-29 20:44:41 +0900

    create configuration of codic API access token
    name: codic.ACCESS_TOKEN

--- src/extension.ts
+++ src/extension.ts
@@ -15,10 +15,9 @@ export function activate(context: vscode.ExtensionContext) {
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.codictranslate', () => {
+        var ACCESS_TOKEN=vscode.workspace.getConfiguration('codic').get('ACCESS_TOKEN');
         // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
